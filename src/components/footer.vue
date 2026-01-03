40e52026fedb423bd541decc21f26fd3e296f030
unknown <dev.tuna.ssm@gmail.com>
2026-01-03 15:59:09 +0700

    first commit

---
+++ src/components/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="border-t border-white mt-30 py-6 md:mt-50">
+    <div class="container mx-auto flex flex-col md:flex-row md:justify-between">
+      <div class="flex flex-row gap-5 mx-auto">
+        <p class="text-center text-white">&copy; 2024 Astralis. All rights reserved.</p>
+      </div>
+      <div class="flex flex-row gap-8 justify-center">
+        <div>
+          <p class="text-center text-white">Devloper</p>
+        </div>
+        <div>
+          <p class="text-center text-white">Partner</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-area">
+    <h1 class="welcome">Welcome to Astralis Dashboard</h1>
+    <div v-if="error" class="error-message">
+      {{ error }}
+    </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo similique exercitationem dicta et rem eos recusandae id expedita ex, deserunt eaque provident aperiam alias. Optio corrupti molestiae neque culpa placeat.
+    </p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: rgb(199, 199, 199);
+  font-size: 3rem;
+  font-family: "Exo 2", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+  }
+  .main-area p {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: rgb(199, 199, 199);
+  }
+</style>
